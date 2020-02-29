14c435a49ff4adfaa9b25768d7e3994f8e13e85d
Jay-Song <sguswhd21@gmail.com>
2020-02-29 14:58:19 -0500

    wrote a test code for multiple function

--- jthesis_main.js
+++ jthesis_main.js
@@ -1,8 +1,10 @@
 var renderer,
     scene,
     camera,
-    type_mesher,
+    arr_type_mesher,
     myCanvas = document.getElementById('myCanvas');
+
+arr_type_mesher = new Array();
 
 //RENDERER
 renderer = new THREE.WebGLRenderer({ canvas: myCanvas, antialias: true });
@@ -37,26 +39,49 @@ gui.add(params, "textField").onFinishChange(function (value) {
     scene.remove(type_mesher.mesh);
     type_mesher.changeText(value);
     scene.add(type_mesher.mesh);
+
+    for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+        scene.remove(arr_type_mesher[mesher_idx].mesh);
+        arr_type_mesher[mesher_idx].changeText(value);
+        scene.add(arr_type_mesher[mesher_idx].mesh);
+    }
 });
 
-//GUI font size
+//GUI font size: changing geometry is required so removing and adding are necessary
 gui.add(params, "fontSize", 12, 150, 1).onChange(function (value) {
     scene.remove(type_mesher.mesh);
     type_mesher.changeTextSize(value);
     scene.add(type_mesher.mesh);
+
+    for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+        scene.remove(arr_type_mesher[mesher_idx].mesh);
+        arr_type_mesher[mesher_idx].changeTextSize(value);
+        scene.add(arr_type_mesher[mesher_idx].mesh);
+    }
 });
 
-//GUI height
+//GUI height: changing geometry is required so removing and adding are necessary
 gui.add(params, "fontThickness", 10, 100, 1).onChange(function (value) {
     scene.remove(type_mesher.mesh);
     type_mesher.changeThickness(value);
     scene.add(type_mesher.mesh);
+
+    for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+        scene.remove(arr_type_mesher[mesher_idx].mesh);
+        arr_type_mesher[mesher_idx].changeThickness(value);
+        scene.add(arr_type_mesher[mesher_idx].mesh);
+    }
 });
 
 //GUI COLOR
 gui.addColor(params, "color").onFinishChange(function (value) {
     type_mesher.changeTextColor(value);
     type_mesher.changeWireframeColor(value);
+
+    for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+        arr_type_mesher[mesher_idx].changeTextColor(value);
+        arr_type_mesher[mesher_idx].changeWireframeColor(value);
+    }
 });
 
 //GUI wire frame: change visibility
@@ -75,15 +100,30 @@ gui.addColor(params, "color").onFinishChange(function (value) {
 // };
 // gui.add(wireframe_btn_obj, 'wireframe');
 
-//GUI wirefram
+//GUI wireframe
 gui.add(params, "wireframe").onChange(function (value){ 
     type_mesher.setWireFrameVisibility(value);
+
+    for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+        arr_type_mesher[mesher_idx].setWireFrameVisibility(value);
+    }
+
 });
 
 //GUI multiple
 gui.add(params, "multiple").onChange(function (value){ 
-    
+    if (value) {
+        for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+            scene.add(arr_type_mesher[mesher_idx].mesh);
+        }
+    }
+    else {
+        for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+            scene.remove(arr_type_mesher[mesher_idx].mesh);
+        }
+    }
 });
+
 //CAMERA
 camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 7000);
 camera.position.z = 100;
@@ -116,9 +156,22 @@ var font = loader.parse(fontJSON);
 type_mesher = new TypeMesher(
     "hello", font, 12, 10,
     0xFF0000,
-    10, 0xFF0000,
+    10, 0xFF0000, false,
     window.innerWidth, window.innerHeight
 );
+
+for(var mesher_idx = 0; mesher_idx < 2; mesher_idx++)
+{
+    arr_type_mesher[mesher_idx] = new TypeMesher(
+        "hello", font, 12, 10,
+        0xFF0000,
+        10, 0xFF0000, false,
+        window.innerWidth, window.innerHeight
+    );
+}
+
+arr_type_mesher[0].mesh.position.x = 50;
+arr_type_mesher[1].mesh.position.x = -50;
 
 type_mesher.setWireFrameVisibility(false);
 scene.add(type_mesher.mesh);
@@ -132,4 +185,10 @@ function render() {
 
     type_mesher.updateMesh(window.innerWidth, window.innerHeight );
     type_mesher.updateWireframeScale();
+
+    for(var mesher_idx = 0; mesher_idx < 2; mesher_idx++)
+    {
+        arr_type_mesher[mesher_idx].updateMesh(window.innerWidth, window.innerHeight );
+        arr_type_mesher[mesher_idx].updateWireframeScale();
+    }
 }
