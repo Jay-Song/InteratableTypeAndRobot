c09870064412bfedebb22ea9819126d4f17defbd
Jay-Song <sguswhd21@gmail.com>
2020-03-01 20:48:29 -0500

    added more meshes with various velocity

--- jthesis_main.js
+++ jthesis_main.js
@@ -40,7 +40,7 @@ gui.add(params, "textField").onFinishChange(function (value) {
     type_mesher.changeText(value);
     scene.add(type_mesher.mesh);
 
-    for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+    for (var mesher_idx = 0; mesher_idx < arr_type_mesher.length; mesher_idx++) {
         scene.remove(arr_type_mesher[mesher_idx].mesh);
         arr_type_mesher[mesher_idx].changeText(value);
 
@@ -56,7 +56,7 @@ gui.add(params, "fontSize", 12, 150, 1).onChange(function (value) {
     type_mesher.changeTextSize(value);
     scene.add(type_mesher.mesh);
 
-    for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+    for (var mesher_idx = 0; mesher_idx < arr_type_mesher.length; mesher_idx++) {
         scene.remove(arr_type_mesher[mesher_idx].mesh);
         arr_type_mesher[mesher_idx].changeTextSize(value);
         if (params.multiple == true) {
@@ -71,7 +71,7 @@ gui.add(params, "fontThickness", 10, 100, 1).onChange(function (value) {
     type_mesher.changeThickness(value);
     scene.add(type_mesher.mesh);
 
-    for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+    for (var mesher_idx = 0; mesher_idx < arr_type_mesher.length; mesher_idx++) {
         scene.remove(arr_type_mesher[mesher_idx].mesh);
         arr_type_mesher[mesher_idx].changeThickness(value);
         if (params.multiple == true) {
@@ -85,7 +85,7 @@ gui.addColor(params, "color").onFinishChange(function (value) {
     type_mesher.changeTextColor(value);
     type_mesher.changeWireframeColor(value);
 
-    for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+    for (var mesher_idx = 0; mesher_idx < arr_type_mesher.length; mesher_idx++) {
         arr_type_mesher[mesher_idx].changeTextColor(value);
         arr_type_mesher[mesher_idx].changeWireframeColor(value);
     }
@@ -96,7 +96,7 @@ gui.add(params, "wireframe").onChange(function (value){
     type_mesher.setWireFrameVisibility(value);
 
     if (params.multiple == true) {
-        for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+        for (var mesher_idx = 0; mesher_idx < arr_type_mesher.length; mesher_idx++) {
             arr_type_mesher[mesher_idx].setWireFrameVisibility(value);
         }
     }
@@ -105,12 +105,12 @@ gui.add(params, "wireframe").onChange(function (value){
 //GUI multiple
 gui.add(params, "multiple").onChange(function (value){ 
     if (value) {
-        for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+        for (var mesher_idx = 0; mesher_idx < arr_type_mesher.length; mesher_idx++) {
             scene.add(arr_type_mesher[mesher_idx].mesh);
         }
     }
     else {
-        for (var mesher_idx = 0; mesher_idx < 2; mesher_idx++) {
+        for (var mesher_idx = 0; mesher_idx < arr_type_mesher.length; mesher_idx++) {
             scene.remove(arr_type_mesher[mesher_idx].mesh);
         }
     }
@@ -147,7 +147,7 @@ type_mesher = new TypeMesher(
 type_mesher.setRotationVelocity(0.005);
 
 //MULTIPLE OBJECT
-var num_of_mesh = 2;
+var num_of_mesh = 20;
 for(var mesher_idx = 0; mesher_idx < num_of_mesh; mesher_idx++)
 {
     arr_type_mesher[mesher_idx] = new TypeMesher(
@@ -156,10 +156,88 @@ for(var mesher_idx = 0; mesher_idx < num_of_mesh; mesher_idx++)
         10, 0xFF0000, false,
         window.innerWidth, window.innerHeight
     );
+
+    var new_idx = mesher_idx % 20;
+    if(new_idx < 10) {
+        arr_type_mesher[mesher_idx].mesh.position.x = (mesher_idx + 1) * 50;
+    }
+    else
+    {
+        arr_type_mesher[mesher_idx].mesh.position.x = -(mesher_idx - 10 + 1) * 50;
+    }
+
+    arr_type_mesher[mesher_idx].setRotationVelocity(Math.random()*0.01 + 0.001);
 }
 
-arr_type_mesher[0].mesh.position.x = 50;
-arr_type_mesher[1].mesh.position.x = -50;
+// % 10 % 2
+for(var mesher_idx = 20; mesher_idx < num_of_mesh + 20; mesher_idx++)
+{
+    arr_type_mesher[mesher_idx] = new TypeMesher(
+        "hello", font, 12, 10,
+        0xFF0000,
+        10, 0xFF0000, false,
+        window.innerWidth, window.innerHeight
+    );
+
+    var new_idx = mesher_idx % 20;
+    if(new_idx < 10) {
+        arr_type_mesher[mesher_idx].mesh.position.x = (mesher_idx-20 + 1) * 50;
+    }
+    else
+    {
+        arr_type_mesher[mesher_idx].mesh.position.x = -((mesher_idx-20) - 10 + 1) * 50;
+    }
+
+    arr_type_mesher[mesher_idx].setRotationVelocity(Math.random()*0.01 + 0.001);
+
+    arr_type_mesher[mesher_idx].mesh.position.y = 50;
+}
+
+for(var mesher_idx = 40; mesher_idx < num_of_mesh + 40; mesher_idx++)
+{
+    arr_type_mesher[mesher_idx] = new TypeMesher(
+        "hello", font, 12, 10,
+        0xFF0000,
+        10, 0xFF0000, false,
+        window.innerWidth, window.innerHeight
+    );
+
+    var new_idx = mesher_idx % 20;
+    if(new_idx < 10) {
+        arr_type_mesher[mesher_idx].mesh.position.x = (mesher_idx-40 + 1) * 50;
+    }
+    else
+    {
+        arr_type_mesher[mesher_idx].mesh.position.x = -((mesher_idx-40) - 10 + 1) * 50;
+    }
+
+    arr_type_mesher[mesher_idx].setRotationVelocity(Math.random()*0.01 + 0.001);
+
+    arr_type_mesher[mesher_idx].mesh.position.y = -50;
+}
+
+var mesher_idx = 60;
+arr_type_mesher[mesher_idx] = new TypeMesher(
+    "hello", font, 12, 10,
+    0xFF0000,
+    10, 0xFF0000, false,
+    window.innerWidth, window.innerHeight
+);
+
+arr_type_mesher[mesher_idx].setRotationVelocity(Math.random()*0.01 + 0.001);
+arr_type_mesher[mesher_idx].mesh.position.y = -50;
+
+var mesher_idx = 61;
+arr_type_mesher[mesher_idx] = new TypeMesher(
+    "hello", font, 12, 10,
+    0xFF0000,
+    10, 0xFF0000, false,
+    window.innerWidth, window.innerHeight
+);
+
+arr_type_mesher[mesher_idx].setRotationVelocity(Math.random()*0.01 + 0.001);
+arr_type_mesher[mesher_idx].mesh.position.y = 50;
+
 
 //SCENE
 scene = new THREE.Scene();
